a92bdfb1f0a33e0e394bf6d427b63de142ace8ca
Ankur12321 <84258110+Ankur12321@users.noreply.github.com>
2024-02-24 20:37:15 -0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,17 +2,17 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6820777476:AAFlSHyMzldSHT-EKpLEklG73RqNqQR4ZRA"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "yt_upload_v5_bot"
 
-    API_ID = ""
+    API_ID = "21970746"
 
-    API_HASH = ""
+    API_HASH = "32deb816dc3874e871b6158673fd3683"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "953851307436-bl66dbgijfunp7n1bph6rbr65cgk10jc.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
     AUTH_USERS = [942731625]
 
